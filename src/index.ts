648b1420615de3feff5b8db2d457d029ebe9dd52
unknown <mbellman@nwe.com>
2022-11-01 16:01:24 -0500

    basic canvas groundwork

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-alert('hi');
+import './body.css';
+
+interface AppState {
+  canvas: HTMLCanvasElement;
+  ctx: CanvasRenderingContext2D;
+}
+
+function createAppState(): AppState {
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+
+  return {
+    canvas,
+    ctx
+  };
+}
+
+function resizeCanvas({ canvas, ctx }: AppState): void {
+  canvas.width = document.body.clientWidth - 100;
+  canvas.height = document.body.clientHeight;
+
+  ctx.fillStyle = '#000';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function main(): void {
+  const state = createAppState();
+
+  document.body.appendChild(state.canvas);
+  
+  resizeCanvas(state);
+
+  window.addEventListener('resize', () => resizeCanvas(state));
+}
+
+main();
