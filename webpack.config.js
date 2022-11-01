25d6e2ceb4d48cacacb5e01121bb12830aa713fd
unknown <mbellman@nwe.com>
2022-11-01 14:51:18 -0500

    initial project structure stuff

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const mode = process.env.npm_lifecycle_script?.includes('webpack-dev-server')
+  ? 'development'
+  : 'production';
+
+module.exports = {
+  devtool: false,
+  mode,
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main-[contenthash].js'
+  },
+  devServer: {
+    open: true,
+    port: 1234
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'swc-loader'
+      },
+      {
+        test: /.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+      inject: true
+    })
+  ],
+  resolve: {
+    extensions: ['.js', '.ts']
+  }
+};
